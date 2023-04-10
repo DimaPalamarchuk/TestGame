d7c82a4bb2ef1247cce13f18e5719c590c23cd7d
Dima <dimapalamarchuk310@gmail.com>
2023-04-11 00:55:16 +0200

    My first game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame, controls
 from gun import Gun
+from pygame.sprite import Group
 
 def run():
     pygame.init()
@@ -7,12 +8,13 @@ def run():
     pygame.display.set_caption("My first game")
     bg_color = (0, 0, 0)
     gun = Gun(screen)
+    bullets = Group()
 
     while True:
-        controls.events(gun)
+        controls.events(screen, gun, bullets)
         gun.update_gun()
-        screen.fill(bg_color)
-        gun.output()
-        pygame.display.flip()
+        bullets.update()
+        controls.update(bg_color, screen, gun, bullets)
+        controls.update_bullets(bullets)
 
 run()
